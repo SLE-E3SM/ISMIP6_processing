3aadf318328916feadf6971928804ece69ce78e0
Matthew Hoffman <mhoffman@lanl.gov>
2023-05-25 10:31:50 -0600

    restructure preprocess script to diff control run
    also general clean up and improvements

--- pre-process-ismip6-run.sh
+++ pre-process-ismip6-run.sh
@@ -6,26 +6,81 @@ EXP=exp13
 # ----------------------------------
 
 # ---- STUFF TO SET ONCE -----------
-BASE_DIR=/lustre/scratch4/turquoise/mhoffman/SLM_Processing_MJH
-ISMIP6_ARCHIVE=/lustre/scratch4/turquoise/hollyhan/ISMIP6-Projections_GCSv4/Full_Cleaned_Projection_Data
+OUTPUT_DIR=/lustre/scratch5/mhoffman/SLM_Processing_MJH
+ISMIP6_ARCHIVE=/lustre/scratch5/mhoffman/ISMIP6_2100_archive
+AISMAPFILE=/usr/projects/climate/mhoffman/SLE-E3SM/ISMIP6_processing/mapfile_polarRank2_to_gaussRank2.nc
+GISMAPFILE=/usr/projects/climate/mhoffman/SLE-E3SM/ISMIP6_processing/mapfile_ismip6_GrIS_Gauss.nc
+STRIDE=5 # stride in years to subsample
 # ----------------------------------
 
 
 # CHANGE NOTHING BELOW HERE
-source /usr/projects/climate/SHARED_CLIMATE/anaconda_envs/load_latest_e3sm_unified_badger.sh
 
-# Regrid data
-exp_path=$BASE_DIR/$IS/${INST}_$ISM/$EXP
+set -e # exit on error
+
+# Select mapping file
+if [[ "$IS" == "AIS" ]]; then
+   MAPFILE=$AISMAPFILE
+elif [[ "$IS" == "GIS" ]]; then
+   MAPFILE=$GISMAPFILE
+else
+   echo "Invalid ice sheet specified."
+   exit
+fi
+echo Using mapfile: $MAPFILE
+
+# set up some paths/variables
+exp_in_path=$ISMIP6_ARCHIVE/$IS/$INST/$ISM/$EXP
+exp_out_path=$OUTPUT_DIR/$IS/${INST}_$ISM/$EXP
 name_base_string=${IS}_${INST}_${ISM}_${EXP}
-echo -e "Starting Regrid using exp_path=$exp_path and name_base_string=$name_base_string\n"
-mkdir -p $exp_path/regridded
-ncremap -m mapfile_polarRank2_to_gaussRank2.nc -i $ISMIP6_ARCHIVE/$IS/$INST/$ISM/$EXP/lithk_${name_base_string}.nc -o $exp_path/regridded/lithk_${name_base_string}_GaussianGrid.nc
-ncremap -m mapfile_polarRank2_to_gaussRank2.nc -i $ISMIP6_ARCHIVE/$IS/$INST/$ISM/$EXP/topg_${name_base_string}.nc -o $exp_path/regridded/topg_${name_base_string}_GaussianGrid.nc
+mkdir -p $exp_out_path/subsampled
+mkdir -p $exp_out_path/regridded
+echo "$BASH_SOURCE"
+echo exp_in_path=$exp_in_path
+echo exp_out_path=$exp_out_path
+echo name_base_string=$name_base_string
 
-# reformat data
-echo -e "\n\nStarting Reformat\n"
-mkdir -p $exp_path/reformatted
-python reformat_SL_inputdata.py $exp_path/regridded/ lithk_${name_base_string}_GaussianGrid.nc topg_${name_base_string}_GaussianGrid.nc
+# ---------
+echo -e "\n\n----- Subsampling data and subtracting control run -----\n"
+# ---------
+# find control run path - assumes only one file will be found
+ctrl_lithk=`ls -1 $ISMIP6_ARCHIVE/$IS/$INST/$ISM/ctrl*/lithk*nc`
 
-mkdir -p $exp_path/SLM_output
+# get section of ctrl that matches time levels of projection.  assume it is the last 86 time levels
+ctrl_20152100=$exp_out_path/subsampled/lithk_${IS}_${INST}_${ISM}_ctrl_2015-2100.nc
+ncks -O -d time,-86, $ctrl_lithk $ctrl_20152100
+
+# calculate anomaly from ctrl
+lithk_anom=$exp_out_path/subsampled/lithk_${name_base_string}_subsampled_anomaly.nc
+ncdiff -O -d time,0,,$STRIDE $exp_in_path/lithk_${name_base_string}.nc $ctrl_20152100 $lithk_anom
+
+# get initial thickness
+lithk_init=$exp_out_path/subsampled/lithk_${name_base_string}_init.nc
+ncks -O -d time,0 $exp_in_path/lithk_${name_base_string}.nc $lithk_init
+ncwa -O -a time $lithk_init ${lithk_init}_notime
+
+# add anomaly to i.c.
+lithk_anom_adj=$exp_out_path/subsampled/lithk_${name_base_string}_subsampled_anomaly_adjusted.nc
+ncbo -O --op_typ=add $lithk_anom ${lithk_init}_notime $lithk_anom_adj
+
+# subsample topg
+topg_subsamp=$exp_out_path/subsampled/topg_${name_base_string}_subsampled.nc
+ncks -O -d time,0,,$STRIDE $exp_in_path/topg_${name_base_string}.nc $topg_subsamp
+
+# --------
+echo -e "\n\n----- Performing remapping of lithk and topg -----\n"
+# --------
+lithk_gauss=$exp_out_path/regridded/lithk_${name_base_string}_GaussianGrid.nc
+ncremap -m $MAPFILE -i $lithk_anom_adj -o $lithk_gauss
+
+topg_gauss=$exp_out_path/regridded/topg_${name_base_string}_GaussianGrid.nc
+ncremap -m $MAPFILE -i $topg_subsamp -o $topg_gauss
+
+# --------
+echo -e "\n\n----- Starting Reformat ------\n"
+# --------
+mkdir -p $exp_out_path/reformatted
+python reformat_SL_inputdata.py $exp_out_path/regridded/ lithk_${name_base_string}_GaussianGrid.nc topg_${name_base_string}_GaussianGrid.nc
+
+mkdir -p $exp_out_path/SLM_output
 echo -e "\nComplete.\n"
