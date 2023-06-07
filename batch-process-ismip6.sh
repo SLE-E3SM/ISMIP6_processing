987eea211f8b993aa463bf22e3118ed6d922aa04
Matthew Hoffman <mhoffman@lanl.gov>
2023-06-06 19:02:41 -0600

    Add batch script for pre-processing and adjust processing script

---
+++ batch-process-ismip6.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#SBATCH --time=0:60:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --qos=debug
+#SBATCH --reservation=debug
+
+# a python env is needed.  may need/want to change this.
+#source /usr/projects/climate/mhoffman/compass/main/load_dev_compass_1.2.0-alpha.5_chicoma-cpu_gnu_mpich_albany.sh
+
+nprocs=128
+
+IS=AIS
+ISMIP6_ARCHIVE=/lustre/scratch5/mhoffman/ISMIP6_2100_archive
+
+
+AIS_STD_EXP_LIST="exp05 exp06 exp07 exp08 exp09 exp10 exp12 exp13"
+EXP_LIST=$AIS_STD_EXP_LIST
+
+i=0
+ii=0
+((maxproc=nprocs-1))
+echo $maxproc
+for d1 in $ISMIP6_ARCHIVE/$IS/*/ ; do
+    INST=`basename $d1`
+    for d2 in $ISMIP6_ARCHIVE/$IS/$INST/*/; do
+       ISM=`basename $d2`
+       #for d3 in $ISMIP6_ARCHIVE/$IS/$INST/$ISM/*/; do
+       #   EXP=`basename $d3`
+       for EXP in $EXP_LIST; do
+          if [ -d "$ISMIP6_ARCHIVE/$IS/$INST/$ISM/$EXP" ]; then
+             echo Starting: $i $ii $IS $INST $ISM $EXP
+             ./pre-process-ismip6-run.sh $IS $INST $ISM $EXP && echo "SUCCESS: $i $ii $IS $INST $ISM $EXP" || echo "FAILED: $i $ii $IS $INST $ISM $EXP" &
+             ((ii+=1))
+             if [[ "$i" == $maxproc ]]; then
+                wait
+                i=0
+             else
+                ((i+=1))
+             fi
+          else
+             echo Skipping: $IS $INST $ISM $EXP
+          fi
+       done
+    done
+done
+wait
--- pre-process-ismip6-run.sh
+++ pre-process-ismip6-run.sh
@@ -1,12 +1,25 @@
-# ---- STUFF TO CHANGE EACH RUN ----
+#!/bin/bash
+
+# a python env is needed.  may need/want to change this.
+#source /usr/projects/climate/mhoffman/compass/main/load_dev_compass_1.2.0-alpha.5_chicoma-cpu_gnu_mpich_albany.sh
+
+# This script can be invoked with 4 command-line arguments
+# for ice sheet, institution, ice-sheet model, and experiments.
+# If it is invoked with anything other than 4 arguments,
+# the hard-coded values on the following lines will be used
+# instead.
+
+# ---- Set experiment
 IS=AIS
 INST=DOE
 ISM=MALI
 EXP=exp13
-# ----------------------------------
+# --------------------
+
+set -e # exit on error
 
 # ---- STUFF TO SET ONCE -----------
-OUTPUT_DIR=/lustre/scratch5/mhoffman/SLM_Processing_MJH
+OUTPUT_DIR=/lustre/scratch5/mhoffman/SLM_Processing_MJH2
 ISMIP6_ARCHIVE=/lustre/scratch5/mhoffman/ISMIP6_2100_archive
 AISMAPFILE=/usr/projects/climate/mhoffman/SLE-E3SM/ISMIP6_processing/mapfile_polarRank2_to_gaussRank2.nc
 GISMAPFILE=/usr/projects/climate/mhoffman/SLE-E3SM/ISMIP6_processing/mapfile_ismip6_GrIS_Gauss.nc
@@ -16,7 +29,16 @@ STRIDE=5 # stride in years to subsample
 
 # CHANGE NOTHING BELOW HERE
 
-set -e # exit on error
+if [ "$#" -eq 4 ]; then
+   IS=$1
+   INST=$2
+   ISM=$3
+   EXP=$4
+   mkdir -p $OUTPUT_DIR/$IS/${INST}_$ISM/$EXP
+   exec &> $OUTPUT_DIR/$IS/${INST}_$ISM/$EXP/log.out
+fi
+
+
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo $SCRIPT_DIR
@@ -47,46 +69,55 @@ echo name_base_string=$name_base_string
 # ---------
 echo -e "\n\n----- Subsampling data and subtracting control run -----\n"
 # ---------
-# find control run path - assumes only one file will be found
-ctrl_lithk=`ls -1 $ISMIP6_ARCHIVE/$IS/$INST/$ISM/ctrl*/lithk*nc`
 
-# get section of ctrl that matches time levels of projection.  assume it is the last 86 time levels
+# find control run path - assumes only one file will be found
+if [ -d "$ISMIP6_ARCHIVE/$IS/$INST/$ISM/ctrl_proj_std/" ]; then
+   ctrl_lithk=`ls -1 $ISMIP6_ARCHIVE/$IS/$INST/$ISM/ctrl_proj_std/lithk*nc`
+elif [ -d "$ISMIP6_ARCHIVE/$IS/$INST/$ISM/ctrl_proj/" ]; then
+   ctrl_lithk=`ls -1 $ISMIP6_ARCHIVE/$IS/$INST/$ISM/ctrl_proj/lithk*nc`
+else
+   echo "Unable to find control run"
+   exit 1
+fi
+echo "Found control run file $ctrl_lithk"
+
+echo "get section of ctrl that matches time levels of projection.  assume it is the last 86 time levels"
 ctrl_lithk_20152100=$exp_out_path/preprocessed/lithk_${IS}_${INST}_${ISM}_ctrl_2015-2100.nc
 ncks -O -d time,-86, $ctrl_lithk $ctrl_lithk_20152100
 
-# get initial ctrl thickness
+echo "get initial ctrl thickness"
 lithk_ctrl_init=$exp_out_path/preprocessed/lithk_${name_base_string}_ctrl_init.nc
 ncks -O -d time,0 $ctrl_lithk_20152100 $lithk_ctrl_init
 ncwa -O -a time $lithk_ctrl_init ${lithk_ctrl_init}_notime
 
-# calculate ctrl anomaly over time
+echo "calculate ctrl anomaly over time"
 lithk_ctrl_anom=$exp_out_path/preprocessed/lithk_${name_base_string}_ctrl_anomaly.nc
 ncdiff -O $ctrl_lithk_20152100 ${lithk_ctrl_init}_notime $lithk_ctrl_anom
 
-# remove ctrl anomaly from projection
+echo "remove ctrl anomaly from projection"
 lithk_anom_adj=$exp_out_path/preprocessed/lithk_${name_base_string}_anomaly_adjusted.nc
 ncdiff -O $exp_in_path/lithk_${name_base_string}.nc $lithk_ctrl_anom $lithk_anom_adj
 
-# ensure no negative thickness!
+echo "ensure no negative thickness!"
 lithk_anom_adj_cln=$exp_out_path/preprocessed/lithk_${name_base_string}_anomaly_adjusted_cleaned.nc
 ncap2 -O -s "where(lithk<0.0) lithk=0.0" $lithk_anom_adj $lithk_anom_adj_cln
 
-# add bed topo so we can calculate grounded ice
+echo "add bed topo so we can calculate grounded ice"
 ncks -A $exp_in_path/topg_${name_base_string}.nc $lithk_anom_adj_cln
 
-# subsample anomaly adjusted file
+echo "subsample anomaly adjusted file"
 lithk_subsamp=$exp_out_path/preprocessed/lithk_${name_base_string}_preprocessed.nc
 ncks -O -d time,0,,$STRIDE $lithk_anom_adj_cln $lithk_subsamp
 
-# only keep grounded ice
+echo "only keep grounded ice"
 grdthk_subsamp=$exp_out_path/preprocessed/grdice_${name_base_string}_preprocessed.nc
 ncap2 -O -s "where(lithk*910/1028+topg<0) lithk=0.0" $lithk_subsamp $grdthk_subsamp
 
-# only need initial topg
+echo "subsample topg - only need initial topg"
 topg_subsamp=$exp_out_path/preprocessed/topg_${name_base_string}_preprocessed.nc
 ncks -O -d time,0 $exp_in_path/topg_${name_base_string}.nc $topg_subsamp
 
-# calc SLC for ctrl, projection, and anomaly-adjusted-cleaned proj
+echo "calc SLC for ctrl, projection, and anomaly-adjusted-cleaned proj"
 python $SCRIPT_DIR/calc_SLR.py --lithk $ctrl_lithk_20152100 --topg $exp_in_path/topg_${name_base_string}.nc --out $exp_out_path/preprocessed/slc-ctrl.nc
 python $SCRIPT_DIR/calc_SLR.py --lithk $exp_in_path/lithk_${name_base_string}.nc --topg $exp_in_path/topg_${name_base_string}.nc --out $exp_out_path/preprocessed/slc-proj.nc
 python $SCRIPT_DIR/calc_SLR.py --lithk $lithk_anom_adj_cln --topg $exp_in_path/topg_${name_base_string}.nc --out $exp_out_path/preprocessed/slc-proj-adj.nc
@@ -96,7 +127,7 @@ echo -e "\n\n----- Performing remapping of lithk and topg -----\n"
 # --------
 grdthk_gauss=$exp_out_path/regridded/grdthk_${name_base_string}_GaussianGrid.nc
 ncremap -m $MAPFILE -i $grdthk_subsamp -o $grdthk_gauss
-# set fill value to 0 and remove attribute to leave 0 values where there is no ice
+echo "set fill value to 0 and remove attribute to leave 0 values where there is no ice"
 ncatted -a _FillValue,,o,f,0.0 $grdthk_gauss
 ncatted -a _FillValue,,d,, $grdthk_gauss
 
@@ -109,6 +140,7 @@ echo -e "\n\n----- Starting Reformat ------\n"
 mkdir -p $exp_out_path/reformatted
 python $SCRIPT_DIR/reformat_SL_inputdata.py $exp_out_path/regridded/ $grdthk_gauss $topg_gauss
 
+echo "set up run directory"
 mkdir -p $exp_out_path/SLM_run/OUTPUT_SLM
 cp namelist.sealevel $exp_out_path/SLM_run
 cp runslm $exp_out_path/SLM_run
